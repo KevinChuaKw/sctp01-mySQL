78b490a770f9f570e2bd7a29c9bb28b4e664442d
Kevin Chua <Kevin_chua3@hotmail.com>
2024-02-04 16:11:22 +0000

    Setting up project

---
+++ 06-mySQL-project/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql2 = require('mysql2/promise');
+const dotenv = require('dotenv');
+const hbs = require('hbs');
+const wax = require('wax-on');
+const handlebarsHelpers = require('handlebars-helpers')({
+    'handlebars':hbs.handlebars
+}); 
+dotenv.config();
+let app = express();
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'hbs');
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
+
+
+
+async function main() {
+    // create a MySQL connection
+    const connection = await mysql2.createConnection({
+        host: process.env.DB_HOST, // server: URL or IP address
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD
+    });
+
+
+}
+main();
+
+app.listen(3000, () => {
+    console.log("server has started");
+});
